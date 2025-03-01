0a0ec9ce10799fbc31e1f63f1d87f485cae04e7b
raza1315 <razasfs2003@gmail.com>
2025-03-01 15:14:19 +0530

    server sample done

---
+++ index.js
@@ -0,0 +1,11 @@
+const express =require("express");
+const cors=require("cors");
+const app=express();
+app.use(express.json());
+app.use(cors());
+app.listen(3000,()=>{
+console.log("server running on port 3000");
+})
+app.get("/",(req,res)=>{
+res.status(200).json("server is up and running");
+})
